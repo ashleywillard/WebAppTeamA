585dab14807a434689eab6ec7ab47fca3e4b6939
andimitre <amitre@pegasus.rutgers.edu>
2014-07-02 22:54:11 -0700

    Update wp-config.php

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -22,7 +22,7 @@ define('DB_NAME', 'wpdb');
 define('DB_USER', 'wpuser');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'adrian11');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
